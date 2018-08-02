b93cec0662ce283c1ea72f19a87c22eb72f4fca2
Deny Satrio <denysatrio1@gmail.com>
2018-08-02 14:25:09 +0700

    change variable

--- binary_search.php
+++ binary_search.php
@@ -1,26 +1,26 @@
 <?php
-  function binarySearch($arr, $l, $r, $x) {
-      if ($r >= $l) {
-        $mid = $l + ($r - $l) / 2;
-        if ($arr[$mid] == $x) {
+  function BinarySearch($array, $bawah, $atas, $x) {
+      if ($atas >= $bawah) {
+        $mid = $bawah + ($atas - $bawah) / 2;
+        if ($array[$mid] == $x) {
             return floor($mid);
           }
 
-        if ($arr[$mid] > $x) {
-            return binarySearch($arr, $l, $mid - 1, $x);
+        if ($array[$mid] > $x) {
+            return BinarySearch($array, $bawah, $mid - 1, $x);
           }
-        return binarySearch($arr, $mid + 1, $r, $x);
+        return BinarySearch($array, $mid + 1, $atas, $x);
       }
     return -1;
   }
 
-  $arr = array(2, 3, 4, 10, 40);
-  $n = count($arr);
+  $array = array(2, 3, 4, 10, 40);
+  $n = count($array);
   $x = 10;
-  $result = binarySearch($arr, 0, $n - 1, $x);
-  if(($result == -1)){
+  $hasil = BinarySearch($array, 0, $n - 1, $x);
+  if(($hasil == -1)){
     echo "Element is not present in array";
   } else {
-    echo "Element is present at index ",$result;
+    echo "Element is present at index ",$hasil;
   }
 ?>
